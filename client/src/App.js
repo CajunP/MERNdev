c0c0b418595dec62f6f831e6dbf7a99c53f3f40c
JuliJarazo <julian@chazki.com>
2020-02-13 12:14:21 -0300

    React con auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
 import React, {Fragment} from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
-import Navbar from '../src/components/layouts/Navbar';
-import Landing from '../src/components/layouts/Landing';
-import Login from '../src/components/auth/Login';
-import Register from '../src/components/auth/Register';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layouts/Alert';
+//Redux
+import {Provider} from 'react-redux';
+import store from './store';
 
 const App = () => (
-    <Router>
-        <Fragment>
-            <Navbar />
-            <Route exact path="/" component={Landing} />
-        </Fragment>
-        <section className="container">
-            <Switch>
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-            </Switch>
-        </section>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar />
+                <Route exact path="/" component={Landing} />
+            </Fragment>
+            <section className="container">
+                <Alert />
+                <Switch>
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/login" component={Login} />
+                </Switch>
+            </section>
+        </Router>
+    </Provider>
 );
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, {Fragment, useState} from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {setAlert} from '../../actions/alert';
+import {register} from '../../actions/auth';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({setAlert, register}) => {
     const [formData, setFromData] = useState({
         name: '',
         email: '',
@@ -14,8 +18,8 @@ const Register = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-        if(password !== password2) console.log('Passwords do not match');
-        else console.log('Success'); 
+        if(password !== password2) setAlert('Passwords do not match', 'danger');
+        else register({name, email, password}); 
     };
 
     return (
@@ -76,4 +80,9 @@ const Register = () => {
     )
 };
 
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired
+};
+
+export default connect(null, {setAlert, register})(Register);
